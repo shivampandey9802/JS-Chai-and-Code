184be59504ae287eca05b5ac7afae1b810b7fd4f
Shivam Pandey <156832926+shivampandey9802@users.noreply.github.com>
2024-10-27 17:52:18 +0000

    datatype

---
+++ 01_Basic/datatype.js
@@ -0,0 +1,19 @@
+//Primitive
+
+// 7 types : String , Number , Boolean , null , 
+// undefined , Symbol , BigInt
+
+
+
+// Reference (Non primitive)
+
+// Array , Objects , Functions
+
+const heros = ['shaktiman' , 'superman'];
+let myObj = {
+    name : "shivam", 
+    activity : 'study',
+}
+const f = () => {
+    console.log("Hello");
+}
