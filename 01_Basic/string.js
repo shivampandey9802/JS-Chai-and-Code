b8a3b49b03a588b293d9b65cb88ab83fa8d9dec9
Shivam Pandey <156832926+shivampandey9802@users.noreply.github.com>
2024-10-28 11:23:09 +0000

    string

---
+++ 01_Basic/string.js
@@ -0,0 +1,33 @@
+const s = "shivam";
+console.log(s);
+
+
+const gameName = new String(s); // created in heap using new key word
+// console.log(gameName);
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase()); // convert string into uppercase
+
+// console.log(gameName.charAt(1)); // finding character which is present at that index
+
+// console.log(gameName.indexOf('h')) // finding index passing character 
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(1 , 4);
+// console.log(anotherString);
+
+
+// const stringWithSpace = "   shivam   ";
+// console.log(stringWithSpace);
+// console.log(stringWithSpace.trim());
+
+
+// const url = "https://shivam.com";
+// console.log(url.replace('.' , '@'));
+
+
