6320bf48876cb64ce6466f4a061b05b0484176ec
Shivam Pandey <156832926+shivampandey9802@users.noreply.github.com>
2024-10-28 12:16:56 +0000

    date

---
+++ 01_Basic/datesIn.js
@@ -0,0 +1,24 @@
+// Date
+
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+
+
+// Create your Own date
+// let myCreatedDate = new Date(2024 , 0 , 23);
+// console.log(myCreatedDate.toDateString());
+
+// Time Stamp
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+
+
+// let newDate = new Date();
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+
