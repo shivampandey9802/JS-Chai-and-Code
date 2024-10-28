3fa2dfa101255033cb761fc0b3cd34c5aa912857
Shivam Pandey <156832926+shivampandey9802@users.noreply.github.com>
2024-10-28 11:52:42 +0000

    number and Math

---
+++ 01_Basic/numberAndMath .js
@@ -0,0 +1,32 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2)); // give 2 number after decimal eg. 100.00
+
+// const otherNumber = 123.8996;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 100000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ***************************************************************************************************
+// Maths
+
+// console.log(Math);
+// console.log(Math.abs(-2));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.1));
+// console.log(Math.floor(4.1));
+console.log(Math.random()); // Range of value/ [0,1)
+
+
+
+
+
+
