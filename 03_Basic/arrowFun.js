c0d8c0aa6995fd3f4af7f98a5e8506c80b6c0f9a
Shivam Pandey <156832926+shivampandey9802@users.noreply.github.com>
2024-10-29 18:04:15 +0000

    arrow Function

---
+++ 03_Basic/arrowFun.js
@@ -0,0 +1,36 @@
+// Arrow Function
+
+// const user = {
+//     username : "shivam",
+//     price : 999,
+
+//     welcomeMessage : function() {
+//         console.log(`${this.username} , welcome to website `);
+//     }
+// }
+
+// user.welcomeMessage();
+// user.username = "sam";
+// user.welcomeMessage();
+
+// const chai = () => {
+//     let username = 'shivam';
+//     console.log(username);
+// }
+// chai();
+
+// const addTwo = (num1 , num2) => {
+//     return num1 + num2;
+// }
+
+// console.log(addTwo(2 , 3));
+
+const addTwo = (num1 , num2) => num1 + num2;
+
+console.log(addTwo(2 , 3));
+
+// If we use curly bracket then we have to use return keyword and if we won't then don't need to write return keyword
+
+const myArr = [2 , 3 , 4 ,5];
+// myArr.forEach((item) => console.log(item));
+// myArr.map((item) => console.log(item));
